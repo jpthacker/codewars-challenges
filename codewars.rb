5e629c53b7e8f86534f464c222a8c2e29595d3ec
Jack Thacker <jackthacker89@gmail.com>
2023-02-28 17:23:16 +0000

    Added extra challenge (challenge 2)

--- codewars.rb
+++ codewars.rb
@@ -20,6 +20,31 @@ def generateHashtag(str)
 end
 
 # CODEWARS 2
+# URL - https://www.codewars.com/kata/54a91a4883a7de5d7800009c/train/ruby
+
+# Your job is to write a function which increments a string, to create a new string.
+
+# If the string already ends with a number, the number should be incremented by 1.
+# If the string does not end with a number. the number 1 should be appended to the new string.
+# Examples:
+
+# foo -> foo1
+
+# foobar23 -> foobar24
+
+# foo0042 -> foo0043
+
+# foo9 -> foo10
+
+# foo099 -> foo100
+
+# Attention: If the number has leading zeros the amount of digits should be considered.
+
+def increment_string(input)
+  #your code here
+end
+
+# CODEWARS 3
 # URL - https://www.codewars.com/kata/583203e6eb35d7980400002a/ruby
 
 # Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
@@ -45,7 +70,7 @@ def count_smileys(arr)
   #your code here
 end
 
-#  CODEWARS 3
+#  CODEWARS 4
 #  URL - https://www.codewars.com/kata/51b62bf6a9c58071c600001b/train/ruby
 
 #  Create a function taking a positive integer between 1 and 3999 (both included) as its parameter and returning a string containing the Roman Numeral representation of that integer.
